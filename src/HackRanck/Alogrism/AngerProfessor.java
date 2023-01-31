3ef006694d594489bfbb9dd26bc22fd9b2cc5782
macpro <nebiyugirma7497@yahoo.com>
2023-01-31 13:25:55 -0800

    update

---
+++ src/HackRanck/Alogrism/AngerProfessor.java
@@ -0,0 +1,13 @@
+package HackRanck.Alogrism;
+
+import java.util.List;
+
+public class AngerProfessor {
+    public static void main(String[] args) {
+
+    }
+
+    public static String angerpROFESSOR(int k , List<Integer> a){
+
+    }
+}
--- src/HackRanck/Alogrism/UtopianTree.java
+++ src/HackRanck/Alogrism/UtopianTree.java
@@ -1,4 +1,18 @@
 package HackRanck.Alogrism;
 
 public class UtopianTree {
+    public static void main(String[] args) {
+        System.out.println(6);
+    }
+    public static int utopianTree(int n){
+        int height=1;
+        for (int i=1;i<=n;i++){
+            if(i%2==1){
+                height*=2;
+            }else {
+                height+=1;
+            }
+        }
+        return height;
+    }
 }
